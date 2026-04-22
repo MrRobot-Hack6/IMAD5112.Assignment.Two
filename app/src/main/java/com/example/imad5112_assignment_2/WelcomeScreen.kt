5fd7c5a13918e23646c8ab37f059073be4fe009c
ArmandSRieck <rieckarmand@gmail.com>
2026-04-22 12:56:08 +0200

    WelcomeScreen.kt added code. Assigned ID, Declared Variables and Initialized Start Button. Set onClickListener to button to move to Main Screen.

--- app/src/main/java/com/example/imad5112_assignment_2/WelcomeScreen.kt
+++ app/src/main/java/com/example/imad5112_assignment_2/WelcomeScreen.kt
@@ -1,6 +1,8 @@
 package com.example.imad5112_assignment_2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,11 +10,27 @@ import androidx.core.view.WindowInsetsCompat
 
 class WelcomeScreen : AppCompatActivity() {
 
+    //Using lateinit Initialize the button variable
+
+    lateinit var btnStart : Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_welcome_screen)
+
+        // Initializing the button variable by finding it in the layout
+
+        btnStart = findViewById(R.id.btnStart)
+
+        // Setting an onClickListener to the button to navigate to the MainActivity when clicked
+
+        btnStart.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
